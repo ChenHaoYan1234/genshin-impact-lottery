473182aeb25c8d264550abd59171f83bf859106d
ChenHaoYan <chanhaoyan@126.com>
2023-12-30 22:04:43 +0800

    将改用PySide6

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-from MainWindow import Ui_MainWindow
-from PyQt6.QtWidgets import QApplication
 import sys
+
+from PySide6.QtWidgets import QApplication
+
+from MainWindow import Ui_MainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
