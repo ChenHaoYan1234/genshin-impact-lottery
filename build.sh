dcf171d48fe1cefcc456f195a59f6d6f16452803
CHenHaoYan1234 <chanhaoyan@126.com>
2024-03-18 17:08:29 +0800

    update build script

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 pyinstaller -F \
             -w \
             -i ./resource/icon.ico \
-            --upx-dir ./utils/upx-4.2.1-amd64_linux/ \
+            --upx-dir ./utils/upx-4.2.2-amd64_linux/ \
             ./src/main.py
